7468dec53b36028c5532cc0c6127a12879a7da32
Víctor Hugo Valle Castillo <victouk@gmail.com>
2024-05-20 23:51:19 -0400

    Feature - Improve the app with a timer (#3)
    * Added lodash module.
    
    * Added a timer to finish the game and a reset feature.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 
 import {Game} from './src/components/Game';
 
+type InitialStateProps = {
+  gameId: number;
+};
+
 export const App = (): React.JSX.Element => {
-  return <Game randomNumberCount={6} />;
+  // Small hack to reset the game (re-assign the ID to remount it).
+  const initialState = useMemo(
+    () => ({
+      gameId: 1,
+    }),
+    [],
+  );
+
+  const [initialStateValues, setInitialStateValues] =
+    useState<InitialStateProps>(initialState);
+
+  const resetGame = () =>
+    setInitialStateValues(prevState => ({
+      ...prevState,
+      gameId: prevState.gameId + 1,
+    }));
+
+  return (
+    <Game
+      key={initialStateValues.gameId}
+      randomNumberCount={6}
+      initialSeconds={10}
+      onPlayAgain={resetGame}
+    />
+  );
 };
