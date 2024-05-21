302e9ca209f08f00b77b5809e208ecf4cd5004ac
Víctor Hugo Valle Castillo <victouk@gmail.com>
2024-05-20 21:22:34 -0400

    Moved the random number to its own component and improved the basic game feature.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,37 +1,106 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import PropTypes from 'prop-types';
+import React, {useState, useMemo, useCallback} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import {RandomNumber} from './RandomNumber';
 
 type GameProps = {
   randomNumberCount: number;
 };
 
 export const Game = ({randomNumberCount}: GameProps) => {
-  const randomNumbers = Array.from({length: randomNumberCount}).map(
-    () => 1 + Math.floor(10 * Math.random()),
+  const initialState = {
+    numberIds: [],
+  };
+
+  const [initialStateValues, setInitialStateValues] = useState<{
+    numberIds: number[];
+  }>(initialState);
+
+  // Use `useMemo` to memoize the `randomNumbers` and `target` (it ensures that these values are only recalculated if randomNumberCount changes).
+  const randomNumbers = useMemo(
+    () =>
+      Array.from({length: randomNumberCount}).map(
+        () => 1 + Math.floor(10 * Math.random()),
+      ),
+    [randomNumberCount],
   );
 
-  const target = randomNumbers
-    .slice(0, randomNumberCount - 2)
-    .reduce((acc, curr) => acc + curr, 0);
+  const target = useMemo(
+    () =>
+      randomNumbers
+        .slice(0, randomNumberCount - 2)
+        .reduce((acc, curr) => acc + curr, 0),
+    [randomNumbers, randomNumberCount],
+  );
+
+  // Use `useCallback` to prevent unnecessary re-renders.
+  const isNumberDisabled = useCallback(
+    (numberIndex: number) =>
+      initialStateValues.numberIds.indexOf(numberIndex) >= 0,
+    [initialStateValues],
+  );
+
+  const selectNumber = useCallback(
+    (numberIndex: number) =>
+      setInitialStateValues(prevState => ({
+        numberIds: [...prevState.numberIds, numberIndex],
+      })),
+    [],
+  );
+
+  const getGameStatus = useCallback(() => {
+    const selectedNumbersSum = initialStateValues.numberIds.reduce(
+      (acc, curr) => acc + randomNumbers[curr],
+      0,
+    );
+
+    if (selectedNumbersSum === target) {
+      return 'WON';
+    }
+
+    if (selectedNumbersSum > target) {
+      return 'GAME OVER';
+    }
+
+    return 'PLAYING';
+  }, [initialStateValues, randomNumbers, target]);
+
+  const targetPanelStyle = useCallback((gameStatus: string) => {
+    if (gameStatus === 'PLAYING') {
+      return styles.statusPlaying;
+    }
+
+    if (gameStatus === 'WON') {
+      return styles.statusWon;
+    }
+
+    if (gameStatus === 'GAME OVER') {
+      return styles.statusGameOver;
+    }
+  }, []);
+
+  const gameStatus = getGameStatus();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.target}>{target}</Text>
+      <Text style={[styles.target, targetPanelStyle(gameStatus)]}>
+        {target}
+      </Text>
 
       <View style={styles.randomContainer}>
         {randomNumbers.map((randomNumber, index) => (
-          <Text key={index} style={styles.random}>
-            {randomNumber}
-          </Text>
+          <RandomNumber
+            key={index}
+            id={index}
+            number={randomNumber}
+            isDisabled={isNumberDisabled(index) || gameStatus !== 'PLAYING'}
+            onPress={selectNumber}
+          />
         ))}
       </View>
+      <Text>{gameStatus}</Text>
     </View>
   );
-};
-
-Game.propTypes = {
-  randomNumberCount: PropTypes.number.isRequired,
 };
 
 const styles = StyleSheet.create({
@@ -51,12 +120,13 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'space-around',
   },
-  random: {
-    backgroundColor: '#999',
-    width: 100,
-    marginHorizontal: 15,
-    marginVertical: 25,
-    fontSize: 35,
-    textAlign: 'center',
+  statusPlaying: {
+    backgroundColor: '#bbb',
+  },
+  statusWon: {
+    backgroundColor: 'green',
+  },
+  statusGameOver: {
+    backgroundColor: 'red',
   },
 });
